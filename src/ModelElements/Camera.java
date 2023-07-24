57e26c3e8a16c72dbb6223d792ee3b0d5e0e5dc8
StrangeresAnn <lykova_annushka@mail.ru>
2023-07-24 14:03:58 +0400

    Initial commit

---
+++ src/ModelElements/Camera.java
@@ -0,0 +1,9 @@
+package ModelElements;
+
+public class Camera {
+    public Point3D Location;
+    public Angle3D Angle;
+
+    public void Rotate(Angle3D n) {}
+    public void Move(Point3D n) {}
+}
---
+++ src/ModelElements/Scene.java
@@ -0,0 +1,13 @@
+package ModelElements;
+
+public class Scene {
+    public int Id;
+    public PoligonalModel Models;
+    public Flash Flashes;
+    public Type method1(Type n) {
+        return ;
+    }
+    public Type method2(Type n1, Type n2) {
+        return ;
+    }
+}
